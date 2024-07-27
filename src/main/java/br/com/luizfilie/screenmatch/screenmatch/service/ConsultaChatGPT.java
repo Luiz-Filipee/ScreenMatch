9d062a2e27c6f59962f478b4b1967d74173e1d0f
Luiz-Filipee <luizkato7@gmail.com>
2024-07-26 22:37:56 -0400

    feat: integrando api do OpenAI

---
+++ src/main/java/br/com/luizfilie/screenmatch/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,20 @@
+package br.com.luizfilie.screenmatch.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+public class ConsultaChatGPT {
+    public static String obterTraducao(String texto) {
+        OpenAiService service = new OpenAiService("sk-proj-xZoE4u6Y9rBsYuXbaT8JT3BlbkFJVMcckKheT3l8LfsVPi3z");
+
+        CompletionRequest requisicao = CompletionRequest.builder()
+                .model("gpt-3.5-turbo-instruct")
+                .prompt("traduza para o português o texto: " + texto)
+                .maxTokens(1000)
+                .temperature(0.7)
+                .build();
+
+        var resposta = service.createCompletion(requisicao);
+        return resposta.getChoices().get(0).getText();
+    }
+}
